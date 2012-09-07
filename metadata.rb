6f46f93963c454096da019e83634aa35f7beb790
Jay Pipes <jaypipes@gmail.com>
2012-09-07 13:38:31 -0400

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+maintainer          "Jay Pipes"
+maintainer_email    "jaypipes@gmail.com"
+license             "Apache 2.0"
+description         "Cookbook for managing Jenkins Jobs via source control"
+long_description    IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version             "0.1.1"
+
+depends             "jenkins"
---
+++ recipes/checks.rb
@@ -0,0 +1,37 @@
+#
+# Cookbook Name:: jenkins-jobs
+# Recipe:: checks
+#
+# Copyright 2012, Jay Pipes
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+include "jenkins"
+
+git_branch = 'master'
+job_name = "check-chef-repo-#{git_branch}"
+
+job_config = File.join(node[:jenkins][:node][:home], "#{job_name}-config.xml")
+
+jenkins_job job_name do
+  action :nothing
+  config job_config
+end
+
+template job_config do
+  source "check-chef-repo-config.xml"
+  variables :job_name => job_name, :branch => git_branch, :node => node[:fqdn]
+  notifies :update, resources(:jenkins_job => job_name), :immediately
+  notifies :build, resources(:jenkins_job => job_name), :immediately
+end
