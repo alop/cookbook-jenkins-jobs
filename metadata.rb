d632024e225aa93fe1d31378d5cde6de02be4206
John Dewey <john@dewey.ws>
2012-10-11 09:42:36 -0700

    Missing closing tag </project>

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email    "jaypipes@gmail.com"
 license             "Apache 2.0"
 description         "Cookbook for managing Jenkins Jobs via source control"
 long_description    IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version             "0.1.4"
+version             "0.1.5"
 
 recipe              "jenkins-jobs::checks", "Installs jobs into the jenkins server"
 
